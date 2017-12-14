f4479a0932481af514a3fc48604e77b3c406dff8
edgar <strazh.goroda@gmail.com>
2017-12-14 11:31:06 +0200

    Created UI for alternative mongodb client

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,17 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
+import {FormsModule} from '@angular/forms';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        FormsModule,
+        HttpModule
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -13,15 +19,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'SQL Client'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('SQL Client');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to SQL Client!');
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Headers, Http, RequestOptions } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public query: string;
+  public result: string;
+  public title = 'SQL Client';
+  constructor(private http: Http) {
+    this.query = 'SELECT a, b, c\n' +
+      'FROM mycol\n' +
+      'WHERE b > 10\n' +
+      'GROUP BY a\n' +
+      'ORDER BY a ASC\n' +
+      'SKIP 1\n' +
+      'LIMIT 10';
+  }
+  onSubmit() {
+    this.http.get('http://localhost:8080/get?query=' + this.query.replace(/\n/g, ' '))
+      .subscribe((res: any) => {
+        const obj = JSON.parse(res._body);
+        this.result = JSON.stringify(obj, undefined, 4);
+      }, (err: any) => {
+        const obj = JSON.parse(err._body);
+        this.result = obj.message;
+      });
+  }
 }
